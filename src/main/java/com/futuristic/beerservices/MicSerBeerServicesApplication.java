8450eb8884f7c3b95a4d4ccc7c764ef0cc24cc44
aditya <adityaajmera89@gmail.com>
2020-01-25 19:46:47 +0530

    Initial commit, Closes: #1

---
+++ src/main/java/com/futuristic/beerservices/MicSerBeerServicesApplication.java
@@ -0,0 +1,13 @@
+package com.futuristic.beerservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicSerBeerServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicSerBeerServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/futuristic/beerservices/MicSerBeerServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futuristic.beerservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicSerBeerServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
