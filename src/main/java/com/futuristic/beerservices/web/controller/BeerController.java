c3310076a595bd2a4b879d8dfc4cbbf025993280
aditya <adityaajmera89@gmail.com>
2020-01-26 08:28:16 +0530

    adding controller, closes #3

---
+++ src/main/java/com/futuristic/beerservices/web/controller/BeerController.java
@@ -0,0 +1,34 @@
+package com.futuristic.beerservices.web.controller;
+
+import com.futuristic.beerservices.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+/**
+ * aditya created on 26/01/20
+ */
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping({"/{beerId}"})
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
+        //todo: provide implementation
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveBeer(@RequestBody BeerDto beerDto) {
+        //todo: implementation
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping({"/{beerId}"})
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+        //todo: implementation
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
